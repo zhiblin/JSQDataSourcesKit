9d8e493da5021c7d30f6ed5c58be334b6fe07628
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-03 21:37:39 -0700

    update docs

--- build_docs.sh
+++ build_docs.sh
@@ -5,4 +5,5 @@
 # https://github.com/realm/jazzy
 # ------------------------------
 
-jazzy -o ./ --source-directory JSQDataSourcesKit/JSQDataSourcesKit/ --readme JSQDataSourcesKit/JSQDataSourcesKit/README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQDataSourcesKit' -g 'https://github.com/jessesquires/JSQDataSourcesKit'
+jazzy -o ./ --source-directory JSQDataSourcesKit/JSQDataSourcesKit/ --readme JSQDataSourcesKit/README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQDataSourcesKit' -g 'https://github.com/jessesquires/JSQDataSourcesKit'
+open index.html -a Safari
