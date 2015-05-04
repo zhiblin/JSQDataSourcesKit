d59dcc2e4e3d640dd7e80b263f752b36938d959d
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-03 22:01:57 -0700

    update docs, submodule

--- build_docs.sh
+++ build_docs.sh
@@ -4,6 +4,6 @@
 # https://github.com/realm/jazzy
 # ------------------------------
 
-git submodule update
+git submodule update --remote
 
 jazzy -o ./ --source-directory JSQDataSourcesKit/JSQDataSourcesKit/ --readme JSQDataSourcesKit/README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQDataSourcesKit' -g 'https://github.com/jessesquires/JSQDataSourcesKit'
