8b17a29bf6614add3b6ba673e945b86249aae87e
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-03 20:32:07 -0700

    add submodile. add docs script.

---
+++ build_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Docs by jazzy
+#
+# https://github.com/realm/jazzy
+# ------------------------------
+
+jazzy --source-directory ./JSQDataSourcesKit --readme ./JSQDataSourcesKit/README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQDataSourcesKit' -g 'https://github.com/jessesquires/JSQDataSourcesKit'
