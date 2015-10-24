e3e54115b05046fc26c4fdb235c39a46a05314a5
Jesse Squires <jesse.d.squires@gmail.com>
2015-10-24 15:14:58 -0700

    update build script for swift 2.1

--- build_docs.sh
+++ build_docs.sh
@@ -6,4 +6,9 @@
 
 git submodule update --remote
 
-jazzy -o ./ --source-directory JSQDataSourcesKit/JSQDataSourcesKit/ --readme JSQDataSourcesKit/README.md -a 'Jesse Squires' -u 'https://twitter.com/jesse_squires' -m 'JSQDataSourcesKit' -g 'https://github.com/jessesquires/JSQDataSourcesKit'
+jazzy --swift-version 2.1 -o ./ --source-directory JSQDataSourcesKit/JSQDataSourcesKit/ \
+      --readme JSQDataSourcesKit/README.md \
+      -a 'Jesse Squires' \
+      -u 'https://twitter.com/jesse_squires' \
+      -m 'JSQDataSourcesKit' \
+      -g 'https://github.com/jessesquires/JSQDataSourcesKit'
